5f3f057f9144fc4b055456c4777cf4259deab01e
mahmoud Soliman <mesaam17@gmail.com>
2024-02-10 16:38:41 +0200

    Add Driver Validation service

---
+++ src/Api/Features/Drivers/Services/DriverValidationService.cs
@@ -0,0 +1,54 @@
+﻿using Api.DataAccess;
+using Api.Middlewares;
+using Dapper;
+
+namespace Api.Features.Drivers.Services;
+
+public class DriverValidationService(DbContext context, ILogger<DriverValidationService> logger) 
+{
+    
+    public async Task EnsureEmailNotExistAsync(string email, CancellationToken cancellationToken = default!)
+    {
+        var normalizedEmail = email.ToLowerInvariant();
+        var query = "SELECT COUNT(*) FROM Drivers WHERE Email = @Email;";
+        
+        try
+        {
+            using (var connection = context.CreateConnection())
+            { 
+                var count =  await connection.ExecuteAsync(query, new { Email = normalizedEmail });
+                if (count > 0)
+                {
+                    throw new CustomException("Email already exists.");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error occurred while validating email");
+            throw; 
+        }
+    }
+
+    public async Task EnsurePhoneNumberNotExistAsync(string phoneNumber, CancellationToken cancellationToken = default!)
+    {
+        var query = "SELECT COUNT(*) FROM Drivers WHERE PhoneNumber = @Number;";
+        
+        try
+        {
+            using (var connection = context.CreateConnection())
+            { 
+                var count =  await connection.ExecuteAsync(query,  new { PhoneNumber = phoneNumber });
+                if (count > 0)
+                {
+                    throw new CustomException("Phone number already exists.");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error occurred while validating phone number");
+            throw; 
+        }
+    }
+}
