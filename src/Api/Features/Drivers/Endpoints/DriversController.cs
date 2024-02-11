7708937ca1021190cce7deb23df2c6cd981359f0
mahmoud Soliman <mesaam17@gmail.com>
2024-02-11 04:32:45 +0200

    Add Driver command operations

--- src/Api/Features/Drivers/Endpoints/DriversController.cs
+++ src/Api/Features/Drivers/Endpoints/DriversController.cs
@@ -1,17 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Api.Features.Drivers.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Features.Drivers.Endpoints;
 
 [Route("drivers")]
-public sealed class DriversController(ILogger<DriversController> logger) : ControllerBase
+public sealed class DriversController(ILogger<DriversController> logger, ISender sender) : ControllerBase
 {
-    [HttpGet(Name = "blah")]
+    [AllowAnonymous]
+    [HttpPost]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public Object Get()
+    public async Task<ActionResult<CreateDriverResponse>> Create([FromForm] CreateDriverRequest request)
     {
-        logger.LogInformation("Getting blah ................................................................");
-        
-        return Ok("blah!");
+        var createResponse= await sender.Send(request);
+        return Ok(createResponse);
     }
     
+    [AllowAnonymous]
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<UpdateDriverResponse>> Update([FromForm] UpdateDriverRequest request)
+    {
+        var updateResponse = await sender.Send(request);
+        return Ok(updateResponse);
+    }
+
+    [AllowAnonymous]
+    [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<DeleteDriverResponse>> Delete(DeleteDriverRequest request)
+    {
+        var deleteResponse = await sender.Send(request);
+        return Ok(deleteResponse);
+    }
+    
+    [AllowAnonymous]
+    [HttpPost("random-bulk-seed")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<CreateRandomBulkDriversResponse>> CreateRandomBulkDrivers(CreateRandomBulkDriversRequest request)
+    {
+        var createRandomBulkDriversResponse = await sender.Send(request);
+        return Ok(createRandomBulkDriversResponse);
+    }
+
+
 }
