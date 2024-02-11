f09a8b4d4460e1beecc1cfe49b5f5de1de487003
mahmoud Soliman <mesaam17@gmail.com>
2024-02-11 04:29:43 +0200

    Change DriverValidationService.cs to handle create and update checks for email and phone number

--- src/Api/Features/Drivers/Services/DriverValidationService.cs
+++ src/Api/Features/Drivers/Services/DriverValidationService.cs
@@ -7,16 +7,32 @@ namespace Api.Features.Drivers.Services;
 public class DriverValidationService(DbContext context, ILogger<DriverValidationService> logger) 
 {
     
-    public async Task EnsureEmailNotExistAsync(string email, CancellationToken cancellationToken = default!)
+    public async Task EnsureEmailNotExistAsync(string email, int? id = null, CancellationToken cancellationToken = default!)
     {
         var normalizedEmail = email.ToLowerInvariant();
-        var query = "SELECT COUNT(*) FROM Drivers WHERE Email = @Email;";
+        var query = "SELECT COUNT(*) FROM Drivers WHERE Email = @Email";
+        
+        object parameters = new
+        {
+            Email = normalizedEmail
+        };
+        
+        if(id.HasValue)
+        {
+            query += " AND Id != @Id";
+            parameters = new
+            {
+                Email = normalizedEmail,
+                Id = id.Value
+            };
+        }
+        query += ";";
         
         try
         {
             using (var connection = context.CreateConnection())
             { 
-                var count =  await connection.ExecuteAsync(query, new { Email = normalizedEmail });
+                var count =  await connection.ExecuteAsync(query,  parameters);
                 if (count > 0)
                 {
                     throw new CustomException("Email already exists.");
@@ -30,15 +46,34 @@ public class DriverValidationService(DbContext context, ILogger<DriverValidation
         }
     }
 
-    public async Task EnsurePhoneNumberNotExistAsync(string phoneNumber, CancellationToken cancellationToken = default!)
+    public async Task EnsurePhoneNumberNotExistAsync(string phoneNumber, int? id = null, CancellationToken cancellationToken = default!)
     {
-        var query = "SELECT COUNT(*) FROM Drivers WHERE PhoneNumber = @Number;";
+        if(string.IsNullOrWhiteSpace(phoneNumber))
+            return;
+        
+        var query = "SELECT COUNT(*) FROM Drivers WHERE PhoneNumber = @PhoneNumber";
+        
+        object parameters = new
+        {
+            PhoneNumber = phoneNumber
+        };
+        
+        if(id.HasValue)
+        {
+            query += " AND Id != @Id";
+            parameters = new
+            {
+                PhoneNumber = phoneNumber,
+                Id = id.Value
+            };
+        }
+        query += ";";
         
         try
         {
             using (var connection = context.CreateConnection())
             { 
-                var count =  await connection.ExecuteAsync(query,  new { PhoneNumber = phoneNumber });
+                var count =  await connection.ExecuteAsync(query,  parameters);
                 if (count > 0)
                 {
                     throw new CustomException("Phone number already exists.");
