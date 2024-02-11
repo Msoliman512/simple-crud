806662cfc4f39c17e62e9470b3dadc4097a8420a
mahmoud Soliman <mesaam17@gmail.com>
2024-02-11 18:54:40 +0200

    add configurations wrapper in tests project

---
+++ src/Tests/Api.Test/Wrappers/ConfigurationWrapper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+
+namespace Api.Test.Wrappers;
+
+public interface IConfigurationWrapper
+{
+    string GetConnectionString(string name);
+}
+
+public class ConfigurationWrapper(IConfiguration configuration) : IConfigurationWrapper
+{
+    public string GetConnectionString(string name)
+    {
+        return configuration.GetConnectionString(name) ?? string.Empty;
+    }
+}
+
+
