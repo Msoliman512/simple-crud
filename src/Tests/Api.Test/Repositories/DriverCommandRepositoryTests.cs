2699f8cc279aa16a3c2947babfd54ec1f5a3634e
mahmoud Soliman <mesaam17@gmail.com>
2024-02-11 18:55:25 +0200

    Add drivers command repositories tests

---
+++ src/Tests/Api.Test/Repositories/DriverCommandRepositoryTests.cs
@@ -0,0 +1,42 @@
+
+
+
+public class DriverCommandRepositoryTests
+{
+    // CreateDriver method inserts a new driver into the database and returns the driver ID
+    [Test]
+    public async Task create_driver_inserts_new_driver_and_returns_id()
+    {
+        
+    }
+
+    // UpdateDriver method updates an existing driver in the database and returns true if successful
+    [Test]
+    public async Task update_driver_updates_existing_driver_and_returns_true()
+    {
+    }
+
+    // DeleteDriver method deletes an existing driver from the database and returns true if successful
+    [Test]
+    public async Task delete_driver_deletes_existing_driver_and_returns_true()
+    {
+    }
+
+    // CreateDriver method throws an exception if the database connection fails
+    [Test]
+    public async Task create_driver_throws_exception_if_database_connection_fails()
+    {
+    }
+
+    // UpdateDriver method returns false if the driver ID does not exist in the database
+    [Test]
+    public async Task update_driver_returns_false_if_driver_id_does_not_exist()
+    {
+    }
+
+    // DeleteDriver method returns false if the driver ID does not exist in the database
+    [Test]
+    public async Task delete_driver_returns_false_if_driver_id_does_not_exist()
+    {
+    }
+}
