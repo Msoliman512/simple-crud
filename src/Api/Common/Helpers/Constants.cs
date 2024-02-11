e446f71233b724d643abcda805dfd0b91643a5ba
mahmoud Soliman <mesaam17@gmail.com>
2024-02-11 05:49:31 +0200

    Add Constants.cs file

---
+++ src/Api/Common/Helpers/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.Common.Helpers;
+
+public static class Constants
+{
+    public const int NamesMaxLength = 50;
+    public const int SearchMaxLength = 100;
+    
+    public const string RecommendedSearchPattern = @"^[a-zA-Z0-9\s\-\.\,]*$";
+
+    public static readonly string[] ColumnsNames = { "FirstName", "LastName", "Email", "PhoneNumber" };
+
+}
