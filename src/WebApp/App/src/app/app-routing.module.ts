6855bcc3e63e2b2d60ce37deebbe2b45700ef846
mahmoud Soliman <mesaam17@gmail.com>
2024-02-12 16:41:41 +0200

    rename app.routes to app-routing as project recreated with different settings

--- src/WebApp/App/src/app/app-routing.module.ts
+++ src/WebApp/App/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddDriverComponent } from './add-driver/add-driver.component';
-import { EditDriverComponent } from './edit-driver/edit-driver.component';
 import { HomeComponent } from './home/home.component';
 import { ViewDriverComponent } from './view-driver/view-driver.component';
+import { AddDriverComponent } from './add-driver/add-driver.component';
+import { EditDriverComponent } from './edit-driver/edit-driver.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'Home', pathMatch: 'full'},
@@ -17,5 +17,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
 export class AppRoutingModule { }
