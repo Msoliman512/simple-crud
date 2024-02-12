349aadad2485f2e44ec9c947722bc96d5abba1f0
mahmoud Soliman <mesaam17@gmail.com>
2024-02-12 16:40:51 +0200

    customize app component to present my home component (listing drivers)

--- src/WebApp/App/src/app/app.component.ts
+++ src/WebApp/App/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'App';
+  constructor(private router: Router) { }
+  HomeClick(){
+    this.router.navigate(['Home']);
+  }
 }
