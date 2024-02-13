92e953e1edb619eb53c6f5392a92914f50981b54
mahmoud Soliman <mesaam17@gmail.com>
2024-02-13 05:05:49 +0200

    Implement view driver component

--- src/WebApp/App/src/app/view-driver/view-driver.component.ts
+++ src/WebApp/App/src/app/view-driver/view-driver.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpProviderService } from '../Service/http-provider.service';
+import { WebApiService } from '../Service/web-api.service';
 @Component({
   selector: 'app-view-driver',
-  standalone: true,
-  imports: [],
   templateUrl: './view-driver.component.html',
   styleUrl: './view-driver.component.scss'
 })
-export class ViewDriverComponent {
+export class ViewDriverComponent implements OnInit {
+  driverId: any;
+  driverDetail : any= [];
+
+  constructor(public webApiService: WebApiService, private route: ActivatedRoute, private httpProvider : HttpProviderService) { }
+
+  ngOnInit(): void {
+    this.driverId = this.route.snapshot.params['driverId'];
+    this.getDriverDetailById();
+  }
+
+  getDriverDetailById() {
+    this.httpProvider.getDriverById(this.driverId).subscribe((data : any) => {
+        if (data != null && data.body != null) {
+          var resultData = data.body;
+          if (resultData) {
+            this.driverDetail = resultData;
+          }
+        }
+      },
+      (error :any)=> { });
+  }
 
 }
