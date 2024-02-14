77199660663e05738fa95e50c874158308c55f31
mahmoud Soliman <mesaam17@gmail.com>
2024-02-14 03:39:48 +0200

    fix console bug NG0100 ExpressionChangedAfterItHasBeenCheckedError

--- src/WebApp/App/src/app/upload-task/upload-task.component.ts
+++ src/WebApp/App/src/app/upload-task/upload-task.component.ts
@@ -1,15 +1,15 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import {AbstractControl, FormControl, ValidatorFn, Validators } from '@angular/forms';
-import { LoggingService } from '../Service/logging.service';
+import { Component, ElementRef, ViewChild} from '@angular/core';
+import {AbstractControl, FormControl, ValidatorFn, Validators} from '@angular/forms';
+import {LoggingService} from '../Service/logging.service';
 
 @Component({
   selector: 'app-upload-task',
   templateUrl: './upload-task.component.html',
   styleUrls: ['./upload-task.component.scss']
 })
-export class UploadTaskComponent {
+export class UploadTaskComponent  {
   fileContent: string | ArrayBuffer = '';
-  wordCountMap: Map<string, number> = new Map();
+  wordCountMap: { [word: string]: number } = {}; // changed for map to this to solve NG0100: ExpressionChangedAfterItHasBeenCheckedError
   isUploading: boolean = false;
   uploadProgress: number = 0;
 
@@ -17,21 +17,24 @@ export class UploadTaskComponent {
 
   @ViewChild('fileInput') fileInput!: ElementRef;
 
-  constructor(public loggingService: LoggingService ) { }
+  constructor(
+    public loggingService: LoggingService,
+  ) {
+  }
 
   onFileSelected(event: any) {
     const file: File = event.target.files[0];
     this.loggingService.logEvent('File selected', file.name);
     // Check if no file is selected
     if (!file) {
-      this.fileControl.setErrors({ 'required': true });
+      this.fileControl.setErrors({'required': true});
       this.loggingService.logInfo('No file selected');
       return;
     }
 
     // Check if the file has a valid extension (.txt)
     if (!this.isValidFileExtension(file.name)) {
-      this.fileControl.setErrors({ 'invalidExtension': true });
+      this.fileControl.setErrors({'invalidExtension': true});
       this.loggingService.logInfo('Invalid File selected (Invalid Extension)', file.name);
       this.fileInput.nativeElement.value = ''; // Reset input value
       this.fileContent = ''; // Reset file content
@@ -44,8 +47,8 @@ export class UploadTaskComponent {
     const reader: FileReader = new FileReader();
 
     reader.onload = (e: any) => {
-      this.fileContent = e.target.result;
-      this.countWords();
+            this.fileContent = e.target.result;
+            this.countWords();
     };
 
     reader.onerror = (e: ProgressEvent<FileReader>) => {
@@ -55,14 +58,15 @@ export class UploadTaskComponent {
 
     reader.readAsText(file);
   }
+
   fileValidator(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
       const file = control.value;
       if (!file) {
-        return { 'required': true };
+        return {'required': true};
       }
       if (!this.isValidFileExtension(file.name)) {
-        return { 'invalidExtension': true };
+        return {'invalidExtension': true};
       }
       return null;
     };
@@ -76,24 +80,32 @@ export class UploadTaskComponent {
 
   countWords() {
     const words = this.fileContent.toString().toLowerCase().split(/\s+/).filter(Boolean);
-    if(words.length === 0)
-    {
+    if (words.length === 0) {
       this.fileContent = 'No content';
       this.loggingService.logInfo('Empty File');
     }
-    this.wordCountMap.clear();
+    // Reset wordCountMap object
+    this.wordCountMap = {};
+
+    // Count words
     words.forEach(word => {
-      this.wordCountMap.set(word, (this.wordCountMap.get(word) || 0) + 1);
+      this.wordCountMap[word] = (this.wordCountMap[word] || 0) + 1;
     });
+
     this.isUploading = false; // Set to false after content is loaded
     this.uploadProgress = 0; // Reset progress
   }
+
+  getObjectKeys(obj: any): string[] {
+    return Object.keys(obj);
+  }
+
 
   updateProgress(loaded: number, total: number) {
     if (this.uploadProgress < 100) {
       setTimeout(() => {
         this.uploadProgress = Math.min((loaded / total) * 100, 100);
-        this.updateProgress(loaded, total); 
+        this.updateProgress(loaded, total);
       }, 200);
     }
   }
@@ -101,7 +113,7 @@ export class UploadTaskComponent {
   resetFile() {
     this.fileInput.nativeElement.value = '';
     this.fileContent = '';
-    this.wordCountMap.clear();
+    this.wordCountMap = {};
     this.isUploading = false;
     this.uploadProgress = 0;
     this.loggingService.logEvent('File reset');
